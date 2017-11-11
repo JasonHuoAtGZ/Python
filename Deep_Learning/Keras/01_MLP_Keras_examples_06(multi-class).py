e9eb6ced9ed70df2115d12a7b73cc7859adfdfee
jason <jason_huo1983@126.com>
2017-11-11 13:49:52 +0800

    # add keras CNN examples

--- Deep_Learning/Keras/01_MLP_Keras_examples_06(multi-class).py
+++ Deep_Learning/Keras/01_MLP_Keras_examples_06(multi-class).py
@@ -37,7 +37,7 @@ def baseline_model():
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
-estimator = KerasClassifier(build_fn=baseline_model, nb_epoch=200, batch_size=5, verbose=0)
+estimator = KerasClassifier(build_fn=baseline_model, nb_epoch=200, batch_size=5, verbose=1)
 
 kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
 
