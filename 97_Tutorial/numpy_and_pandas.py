c60206382e4d66a9f1f9ab494e3dffc03fc137b0
JasonHuo <jason_huo1983@126.com>
2021-03-24 18:00:41 +0800

    # 20210317

--- 97_Tutorial/numpy_and_pandas.py
+++ 97_Tutorial/numpy_and_pandas.py
@@ -9,22 +9,19 @@ print("""\
 import numpy as np
 import pandas as pd
 
-
 narray_1 = np.array([[1.1, 2.2, 3.3],
-                   [4.5, 5.6, 6.7]])
+                     [4.5, 5.6, 6.7]])
 
 print(narray_1)
-print(narray_1*10)
+print(narray_1 * 10)
 print(narray_1 + narray_1)
 
 print(narray_1.shape)
 print(narray_1.ndim)
 print(narray_1.dtype)
 
-
 narray_2 = np.zeros((3, 10))
 print(narray_2)
-
 
 print("""\
     ==================================================================
@@ -42,9 +39,90 @@ print(np.dtype(s1.index))
 print(s1[0])
 print(s1.values)
 
-
 for item in s1.index:
-      print(s1[item])
+    print(s1[item])
 
 s2 = pd.Series([1, 2, 3, 4], index=['001', '002', '003', '004'])
 print(s2)
+
+print("""\
+    ==================================================================
+    =============================Indexing=============================
+    =============================20210324=============================
+    ==================================================================
+    """
+      )
+
+df_bank_base = pd.read_csv("C:/Users/jason/Working_Folder/03_Python/bank-full.csv", sep=";", header=0)
+
+print(df_bank_base)
+print(df_bank_base.columns)
+
+df_temp = df_bank_base[['age', 'job']]
+print(df_temp.head(10))
+
+print(df_bank_base.info)
+
+df_temp2 = df_bank_base['job']
+
+print(df_temp2.value_counts())
+
+"""
+for col in df_bank_base.columns:
+    print(df_bank_base[col][:10])
+"""
+
+"""
+numeric_type = ['int16', 'int32', 'int64', 'float32', 'float64']
+df_num = df_bank_base.select_dtypes(include=numeric_type)
+
+df_smy = df_bank_base.describe(percentiles=[0.01, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50 /
+                                            0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 0.99])
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+df_smy_cnt = df_smy.loc['count']
+record_cnt = df_bank_base.shape[0]
+df_smy_cnt = pd.DataFrame(df_smy_cnt / record_cnt)
+df_smy_cnt = df_smy_cnt.rename(columns={'count':'C_rate'})
+df_smy_cnt = df_smy_cnt.transpose()
+
+# df_temp3 = pd.DataFrame(df_smy_cnt, )
+
+df_smy2 = df_smy_cnt.append(df_smy)
+print(df_smy2)
+
+
+numeric_type = ['int16', 'int32', 'int64', 'float32', 'float64']
+df_char = df_bank_base.select_dtypes(exclude=numeric_type)
+
+print(df_char.head(10))
+
+record_cnt = df_char.shape[0]
+
+df_sum = pd.DataFrame(columns=['id_temp'])
+
+for col in df_char.columns:
+    df_temp = pd.DataFrame(df_char[col].value_counts())
+    df_temp[col+'_val'] = df_temp.index
+    df_temp[col+'_p'] = df_temp[col] / record_cnt
+
+    df_temp = df_temp.rename(columns={col:col+'_cnt'})
+    df_temp = df_temp.reset_index(drop=True)
+
+    df_temp['id_temp'] = df_temp.index
+
+    df_sum = pd.merge(df_sum, df_temp, how='outer', on='id_temp')
+
+    print(df_temp)
+
+print(df_sum)
+"""
+from MyPackage.DataExplore import data_explore_num
+from MyPackage.DataExplore import data_explore_char
+
+data_explore_num(df_bank_base)
+data_explore_char(df_bank_base)
---
+++ MyPackage/DataExplore.py
@@ -0,0 +1,66 @@
+"""
+This module is a collection of data exploration for dataframe only
+"""
+import pandas as pd
+
+
+def data_explore_num(df_in):
+
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', None)
+
+    df_smy = df_in.describe(percentiles=[0.01, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50 /
+                                                0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 0.99])
+
+    df_smy_cnt = df_smy.loc['count']
+    record_cnt = df_in.shape[0]
+    df_smy_cnt = pd.DataFrame(df_smy_cnt / record_cnt)
+    df_smy_cnt = df_smy_cnt.rename(columns={'count':'C_rate'})
+    df_smy_cnt = df_smy_cnt.transpose()
+
+    df_smy_cnt = df_smy_cnt.append(df_smy)
+
+    print("======================================================================================>>")
+    print("==================================Percentiles=========================================>>")
+    print(df_smy_cnt)
+    print("======================================================================================>>")
+
+    return df_smy_cnt
+
+
+def data_explore_char(df_in):
+
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', None)
+
+    numeric_type = ['int16', 'int32', 'int64', 'float32', 'float64']
+    df_in = df_in.select_dtypes(exclude=numeric_type)
+
+    record_cnt = df_in.shape[0]
+
+    df_smy_cnt = pd.DataFrame(columns=['id_temp'])
+
+    for col in df_in.columns:
+        df_temp = pd.DataFrame(df_in[col].value_counts())
+        df_temp[col+'_val'] = df_temp.index
+        df_temp[col+'_p'] = df_temp[col] / record_cnt
+
+        df_temp = df_temp.rename(columns={col:col+'_cnt'})
+        df_temp = df_temp.reset_index(drop=True)
+
+        df_temp['id_temp'] = df_temp.index
+
+        df_smy_cnt = pd.merge(df_smy_cnt, df_temp, how='outer', on='id_temp')
+
+        df_smy_cnt.drop(['id_temp'], axis=1)
+
+    print("======================================================================================>>")
+    print("==================================Distribution========================================>>")
+    print(df_smy_cnt)
+    print("======================================================================================>>")
+
+    return df_smy_cnt
